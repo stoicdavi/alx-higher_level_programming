ae4979033c5d0859ae34cced95a4222b99cf1e71
David Nanjila <nanjiladavid2@gmail.com>
2024-01-25 01:25:06 +0300

    updated the code to print alphabet but exclude q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if letter != 101 and letter != 113:
-        print("{}".format(chr(letter)), end="")
+# for letter in range(97, 123):
+#     if letter != 101 and letter != 113:
+#         print("{}".format(chr(letter)), end="")
+for i in range(97, 123):
+    if chr(i) not in ('q', 'e'):
+        print("{}".format(chr(i)), end='')
