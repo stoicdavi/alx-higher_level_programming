db291af57bb348f506f39e18929e70fb0af9663b
David Nanjila <nanjiladavid2@gmail.com>
2024-01-26 23:31:31 +0300

    used alternative

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,5 @@
 #     if letter != 101 and letter != 113:
 #         print("{}".format(chr(letter)), end="")
 for i in range(97, 123):
-    if chr(i) not in ('q', 'e'):
+    if i != 101 and i != 113:
         print("{}".format(chr(i)), end='')
