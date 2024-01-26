7ae1649df045d2e49c6e1b3da4c701c1d631cd11
David Nanjila <nanjiladavid2@gmail.com>
2024-01-26 22:14:57 +0300

    alphabet

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
-# #Write a program that prints all numbers from 0 to 98 in decimal and in hexadecimal 
-# for i in range(0, 99):
-#     print("{:d} = 0x{:x}".format(i, i))
 #!/usr/bin/python3
 for i in range(99):
     print("{} = {}".format(i, hex(i)))
