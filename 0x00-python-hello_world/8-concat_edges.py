80e6151062105a66142e8b992dcb4c831e5dcc57
David Nanjila <nanjiladavid2@gmail.com>
2024-01-26 21:39:32 +0300

    updated the string name

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-text1 = str[39:66] + str[106:112] + str[:6]
-print(text1)
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
