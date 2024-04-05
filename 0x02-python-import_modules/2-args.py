f9d8668a2753b90a216dd0d49f09e54cd2d75e56
David Nanjila <nanjiladavid2@gmail.com>
2024-04-06 00:28:00 +0300

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if arguments == 0:
         print("0 arguments.")
     elif arguments == 1:
-        print("{}: {} argument".format(arguments, sys.argv[1]))
+        print("{}: {} argument:".format(arguments, sys.argv[1]))
     else:
         print("{} arguments:".format(arguments))
-    for i in range(1, arguments + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+        for i in range(1, arguments + 1):
+            print("{}: {}".format(i, sys.argv[i]))
