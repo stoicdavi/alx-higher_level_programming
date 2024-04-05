32addfa3f72f665f15a81ffb7543b106187f9314
David Nanjila <nanjiladavid2@gmail.com>
2024-04-06 00:31:51 +0300

    fix: import the moule correctly

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    arguments = len(sys.argv) - 1
+    from sys import argv
+    arguments = len(argv) - 1
     if arguments == 0:
         print("0 arguments.")
     elif arguments == 1:
-        print("{}: {} argument:".format(arguments, sys.argv[1]))
+        print("{}: {} argument:".format(arguments, argv[1]))
     else:
         print("{} arguments:".format(arguments))
         for i in range(1, arguments + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, argv[i]))
