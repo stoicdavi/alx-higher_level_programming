6883ea5b378c7428e5b543e69ce6e6b603241ab5
David Nanjila <nanjiladavid2@gmail.com>
2024-04-23 13:28:32 +0300

    fix: pycodestyle errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def main():
-    argument_count = len(argv) - 1 # subtract 1 to exclude the script name
+    argument_count = len(argv) - 1
     if argument_count == 0:
         print("0 arguments.")
     elif argument_count == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(argument_count))
+
     for i in range(1, argument_count + 1):
         print("{}: {}".format(i, argv[i]))
+
+
 if __name__ == "__main__":
     main()
