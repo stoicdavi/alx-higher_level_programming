3dc862cf7901238586cf042883bb6a9aa9a57a3b
David Nanjila <nanjiladavid2@gmail.com>
2024-04-23 13:37:13 +0300

    fix: program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+import sys
+
+
+def main():
+    # Start with a total of 0
+    total = 0
+    # Iterate over the arguments excluding the first one (script name)
+    for arg in sys.argv[1:]:
+        # Add the integer value of each argument to the total
+        total += int(arg)
+    # Print the total sum
+    print(total)
+
+
 if __name__ == "__main__":
-    from sys import argv
-    result = 0
-    for i in argv:
-        if argv.index(i) == 0:
-            continue
-        result += int(i)
-    print(result)
+    main()
