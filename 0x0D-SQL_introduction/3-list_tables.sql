bbac715106c539a6a511237eef32f5c0a0183666
David Nanjila <nanjiladavid2@gmail.com>
2024-07-19 04:21:49 +0000

    feat: Write a script that lists all the tables of a database in your MySQL server.
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
--- lists all the tables of a database in your MySQL server
--- The database name will be passed as argument of mysql command
-SHOW TABLES;
+-- lists all the tables of a database in mysql server
+-- database name will be passed as argument of mysql command
+SHOW tables;
