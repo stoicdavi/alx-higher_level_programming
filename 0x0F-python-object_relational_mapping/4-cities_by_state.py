6dab2653906252df0d239dfadb5a460ffe84443c
NetAcad DEVASC <changeme@example.com>
2024-07-30 17:00:21 +0000

    Write a script that lists all cities from the database hbtn_0e_4_usa
    Signed-off-by: NetAcad DEVASC <changeme@example.com>

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" This module lists all the cities from the database
+hbtn_0e_4_usa
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities\
+                INNER JOIN states\
+                ON cities.state_id = states.id\
+                ORDER BY cities.id")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
