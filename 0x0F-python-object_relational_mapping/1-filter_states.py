a3fa3cb292ab89a3009567968552409e13d63b95
NetAcad DEVASC <changeme@example.com>
2024-07-30 16:50:21 +0000

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
    Signed-off-by: NetAcad DEVASC <changeme@example.com>

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that lists all states with a name
+starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
+    cur = conn.cursor()
+    cur.execute("""
+    SELECT * FROM states
+    WHERE name LIKE 'N%'
+    ORDER BY states.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
